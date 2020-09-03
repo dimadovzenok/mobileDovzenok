49bc2cb90347f6e72f4e5750945719d0342931fb
opilane <opilane@HP-CZC6297BBB>
2020-09-03 10:40:09 +0300

    Valgusfoor2

--- mobileDovzenok/mobileDovzenok/Valgusfoor2.xaml.cs
+++ mobileDovzenok/mobileDovzenok/Valgusfoor2.xaml.cs
@@ -17,31 +17,63 @@ namespace mobileDovzenok
             //InitializeComponent();
             Label punane = new Label()
             {
-                Text = "Punane",
-                TextColor = Color.Red,
+                Text = "punane",
+                TextColor = Color.White,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
+            Frame pun = new Frame()
+            {
+                BackgroundColor = Color.Red,
+                Content = punane,
+                CornerRadius = 90,
+                Margin = new Thickness(80, 0, 80, 0)
+            };
+
             Label kollane = new Label()
             {
-                Text = "Kollane",
-                TextColor = Color.Yellow,
+                Text = "kollane",
+                TextColor = Color.White,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
+            };
+            Frame kol = new Frame()
+            {
+                BackgroundColor = Color.Yellow,
+                Content = kollane,
+                CornerRadius = 90,
+                Margin = new Thickness(80, 0, 80, 0)
             };
             Label roheline = new Label()
             {
                 Text = "Roheline",
-                TextColor = Color.Green,
+                TextColor = Color.White,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
+            Frame roh = new Frame()
+            {
+                BackgroundColor = Color.Green,
+                Content = roheline,
+                CornerRadius = 90,
+                Margin = new Thickness(80, 0, 80, 0)
+            };
             StackLayout stackLayout = new StackLayout()
             {
-                Children = { punane, kollane, roheline }
+                Children = { pun, kol, roh }
             };
-            stackLayout.Orientation = StackOrientation.Horizontal;
+            //stackLayout.Orientation = StackOrientation.Horizontal;
             Content = stackLayout;
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
