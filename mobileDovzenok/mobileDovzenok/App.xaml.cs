48f48af65bf6a2c71b86043263ecdb752a4365da
Opilane TTHK <opilane@tthk.ee>
2020-09-03 10:11:08 +0300

    Add project files.

---
+++ mobileDovzenok/mobileDovzenok/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobileDovzenok
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Valgusfoor2(); // стартовая страница
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ mobileDovzenok/mobileDovzenok/Valgusfoor.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobileDovzenok
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Valgusfoor : ContentPage
+    {
+        public Valgusfoor()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            int c=rnd.Next(1, 4);
+            if (c == 1)
+            {
+                pun.BackgroundColor = Color.FromRgb(255, 0, 0);
+                kol.BackgroundColor = Color.Gray;
+                roh.BackgroundColor = Color.Gray;
+            }
+            else if(c==2)
+            {
+                pun.BackgroundColor = Color.Gray;
+                kol.BackgroundColor = Color.FromRgb(255, 255, 0);
+                roh.BackgroundColor = Color.Gray;
+            }
+            else if (c == 3)
+            {
+                pun.BackgroundColor = Color.Gray;
+                kol.BackgroundColor = Color.Gray;
+                roh.BackgroundColor = Color.FromRgb(0, 255, 0);
+            }
+            else if (c == 4)
+            {
+                pun.BackgroundColor = Color.FromRgb(255, 0, 0);
+                kol.BackgroundColor = Color.FromRgb(255, 255, 0);
+                roh.BackgroundColor = Color.FromRgb(0, 255, 0);
+            }
+        }
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+            pun.BackgroundColor = Color.FromRgb(100, 100, 100);
+            kol.BackgroundColor = Color.Gray;
+            roh.BackgroundColor = Color.FromHex("#aaaaa");
+        }
+    }
+}
