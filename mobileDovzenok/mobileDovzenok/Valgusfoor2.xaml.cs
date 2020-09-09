72d43043048b18f51447cdb33ae95306ab76cd9c
opilane <opilane@HP-CZC6297BBB>
2020-09-09 15:31:51 +0300

    valgusfoor2 sisse/valja

--- mobileDovzenok/mobileDovzenok/Valgusfoor2.xaml.cs
+++ mobileDovzenok/mobileDovzenok/Valgusfoor2.xaml.cs
@@ -137,11 +137,22 @@ namespace mobileDovzenok
                 await Task.Run(() => Thread.Sleep(1000));
             }
         }
-        private  void Sisse_Clicked(object sender, EventArgs e)
+        private async  void Sisse_Clicked(object sender, EventArgs e)
         {
+            pun.BackgroundColor = Color.Red;
+            pun.BorderColor = Color.WhiteSmoke;
+            sisse_valja = false;
+            for (int i = 0; i < 100; i++)
+            {
+                punane.FontSize++;
+                await Task.Run(() => Thread.Sleep(1000));
+            }
+
+            
             int c = rnd.Next(1, 4);
             if (c == 1)
             {
+
                 pun.BackgroundColor = Color.Red;
                 kol.BackgroundColor = Color.FromRgb(62, 62, 62);
                 roh.BackgroundColor = Color.FromRgb(62, 62, 62);
